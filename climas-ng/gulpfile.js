cdf7295eb2aa8c5425455b017c78c296f7837810
Daniel Baird <daniel@danielbaird.com>
2014-02-11 14:21:27 +1000

    more functionality in gulpfile

--- climas-ng/gulpfile.js
+++ climas-ng/gulpfile.js
@@ -7,7 +7,8 @@ var plugins = require('gulp-load-plugins')();
 
 
 // -------------------------------------- show all the loaded plugins
-gulp.task('plugins', function(){
+gulp.task('plugins', function() {
+
     console.log('Loaded Plugins:');
     Object.keys(plugins).forEach( function(key) {
         console.log('    ' + key);
@@ -15,19 +16,32 @@ gulp.task('plugins', function(){
 });
 
 // ----------------------------------------------------- default task
-gulp.task('default', function(){
-  // place code for your default task here
-});
+gulp.task('default', ['styles', 'js', 'watch']);
 
 // ------------------------------------------------------ compile css
-gulp.task('styles', function(){
-    gulp.src('climasng/static/css/map.less')
+gulp.task('styles', ['cleancss'], function() {
+    return gulp.src('climasng/src/css/page-*.less')
         .pipe(plugins.less())
         .on('error', gutil.log).on('error', gutil.beep)
         .pipe(plugins.autoprefixer())
         .pipe(gulp.dest('climasng/static/css/'))
         .pipe(plugins.rename({ suffix: '.min' }))
         .pipe(plugins.minifyCss())
-        .pipe(gulp.dest('climasng/static/css/'))
-        ;
+        .pipe(gulp.dest('climasng/static/css/')) ;
+});
+
+// ------------------------------------------------------ compile css
+gulp.task('js', ['cleanjs'], function() {
+    return gulp.src('climasng/src/js/page-*.js')
+        .pipe(gulp.dest('climasng/static/js/')) ;
+});
+
+// ------------------------------------------------- delete built css
+gulp.task('cleancss', function() {
+    return gulp.src(['climasng/static/css/*.css'], {read: false}).pipe(plugins.clean());
+});
+
+// -------------------------------------------------- delete built js
+gulp.task('cleanjs', function() {
+    return gulp.src(['climasng/static/js/*.js'], {read: false}).pipe(plugins.clean());
 });
